6bd72aeec9223f72b777630fcac674dda6f61967
André devkcud Albanese Junior <patommmmm@proton.me>
2024-12-21 05:30:51 -0300

    chore: Add color prop for button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -4,6 +4,7 @@
 
 	type Props = {
 		children: Snippet<[]>;
+		color?: 'normal' | 'primary' | 'secondary';
 		icon?: string;
 		anchor?: string;
 		submit?: boolean;
@@ -11,7 +12,15 @@
 		onclick?: (event: MouseEvent) => void;
 	};
 
-	let { children, icon, anchor, submit = false, bordered = false, onclick }: Props = $props();
+	let {
+		children,
+		color = 'normal',
+		icon,
+		anchor,
+		submit = false,
+		bordered = false,
+		onclick
+	}: Props = $props();
 </script>
 
 <svelte:element
@@ -20,14 +29,21 @@
 	tabindex={0}
 	href={anchor}
 	type={submit === true ? 'submit' : 'button'}
-	class="active:bg-accent inline-flex items-center gap-2 truncate rounded-md px-4 py-2 transition-all hover:scale-105 hover:bg-neutral-800 active:scale-100"
-	class:bg-accent={!bordered}
+	class="inline-flex items-center gap-2 truncate rounded-lg px-4 py-2 transition-all hover:brightness-125 active:scale-95 active:brightness-75"
 	class:border={bordered}
-	class:border-accent={bordered}
+	class:border-accent={bordered && color === 'normal'}
+	class:border-primary={bordered && color === 'primary'}
+	class:border-secondary={bordered && color === 'secondary'}
+	class:text-primary={bordered && color === 'primary'}
+	class:text-secondary={bordered && color === 'secondary'}
+	class:bg-accent={!bordered && color === 'normal'}
+	class:bg-primary={!bordered && color === 'primary'}
+	class:bg-secondary={!bordered && color === 'secondary'}
+	class:text-dark={!bordered && color !== 'normal'}
 	{onclick}
 >
 	{#if icon}
-		<Icon {icon} class="inline size-6" />
+		<Icon {icon} class="inline size-4" />
 	{/if}
 
 	{@render children()}
