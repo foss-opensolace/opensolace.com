acdd657a5cdcf2927b4cff9cddb8141b41ea6498
André devkcud Albanese Junior <patommmmm@proton.me>
2024-12-21 01:19:33 -0300

    feat: Create button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		children: Snippet<[]>;
+		icon?: string;
+		anchor?: string;
+		submit?: boolean;
+		bordered?: boolean;
+		onclick?: (event: MouseEvent) => void;
+	};
+
+	let { children, icon, anchor, submit = false, bordered = false, onclick }: Props = $props();
+</script>
+
+<svelte:element
+	this={anchor ? 'a' : 'button'}
+	role="button"
+	tabindex={0}
+	href={anchor}
+	type={submit === true ? 'submit' : 'button'}
+	class="inline-flex items-center gap-2 truncate rounded-md px-4 py-2 transition-all hover:scale-105 hover:bg-neutral-800 active:scale-100 active:bg-neutral-900"
+	class:bg-neutral-900={!bordered}
+	class:border={bordered}
+	class:border-neutral-900={bordered}
+	{onclick}
+>
+	{#if icon}
+		<Icon {icon} class="inline size-6" />
+	{/if}
+
+	{@render children()}
+</svelte:element>
